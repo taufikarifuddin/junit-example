6d2938eb6582d1ccd43726a61159d1c26504c351
TanoSutano <58139137+TanoSutano@users.noreply.github.com>
2020-03-10 15:53:53 +0700

    Add files via upload

--- CommonUtilTest.java
+++ CommonUtilTest.java
@@ -10,11 +10,27 @@ public class CommonUtilTest {
   }
 
   @Test
-  public void testMethod1(){Assert.assertEquals("Prime Number", CommonUtil.primeNumberValidation(2));}
+  public void testMethodPrimeNumber(){Assert.assertEquals("Prime Number", CommonUtil.primeNumberValidation(2));}
 
   @Test
-  public void testMethod2(){Assert.assertEquals("Ganjil", CommonUtil.evenOrOdd(3));}
+  public void testMethodNotPrimeNumber(){Assert.assertEquals("Not Prime Number", CommonUtil.primeNumberValidation(1));}
 
   @Test
-  public void testMethod(){Assert.assertEquals(0, CommonUtil.div(9, "asd"));}
+  public void testMethodEvenNumber(){Assert.assertEquals("Ganjil", CommonUtil.evenOrOdd(3));}
+
+  @Test
+  public void testMethodOddNumber(){Assert.assertEquals("Genap", CommonUtil.evenOrOdd(2));}
+
+  @Test
+  public void testMethodDivByString(){Assert.assertEquals(0, CommonUtil.div(9, "asd"));}
+
+  @Test
+  public void testMethodDivByZero(){Assert.assertEquals(0, CommonUtil.div(9, "0"));}
+
+  @Test
+  public void testMethodDivSuccess(){Assert.assertEquals(12, CommonUtil.div(144,"12"));}
+
+
+
+
 }
